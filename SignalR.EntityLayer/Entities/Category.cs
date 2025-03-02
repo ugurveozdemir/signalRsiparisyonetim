39c7975f4ee686ba7f6b01a2aec824326239466a
Uğur Özdemir <ugur3ozdemir@gmail.com>
2025-03-03 00:22:51 +0300

    katmanlar olusturuldu, entityler olusturuldu, servisler yazildi

---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLater.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
