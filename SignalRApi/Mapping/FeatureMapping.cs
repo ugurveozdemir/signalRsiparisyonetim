0f3041127d4ef6a4c404fdb016b8cb70ebfe7105
Uğur Özdemir <ugur3ozdemir@gmail.com>
2025-03-09 19:03:55 +0300

    mappingler tamamlandi, controller yazilmaya baslandi

---
+++ SignalRApi/Mapping/FeatureMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.FeatureDto;
+using SignalR.EntityLater.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class FeatureMapping : Profile
+    {
+        public FeatureMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeaureDto>().ReverseMap();
+            CreateMap<Feature, GetFeatureDto>().ReverseMap();
+        }
+    }
+}
---
+++ SignalRApi/Mapping/ProductMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLater.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class ProductMapping : Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, GetProductDto>().ReverseMap();
+        }
+    }
+}
